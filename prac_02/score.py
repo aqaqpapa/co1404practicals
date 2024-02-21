df4de7cf7ce24e347bb068de1bb65eb69a82661f
Hjyheart <764796124@qq.com>
2024-02-22 01:50:21 +0800

    go

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,7 +1,11 @@
-
+import random
 def main():
   score = float(input("Enter score: "))
   print(outcome(score))
+  print("random grades")
+  random_grades = random.randint(0, 100)
+  print(random_grades)
+  print(outcome(random_grades))
 
 def outcome(s):
   if s<0 or s>100:
