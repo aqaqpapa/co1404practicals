3e93a8ea1c8b34790682d5a228fba6e04be9a909
Hjyheart <764796124@qq.com>
2024-03-30 21:17:14 +0800

    go

--- prac_08/KivyDemos-master/squaring.py
+++ prac_08/KivyDemos-master/squaring.py
@@ -1,14 +1,13 @@
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
+
 
 
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
